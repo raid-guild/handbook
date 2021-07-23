3df8bfb17cdbce06b2632b1e6766c4142edf633e
jonathanprozzi <jonathanprozzi@gmail.com>
2021-07-22 23:44:52 -0400

    added discord-bots with some slight formatting edits for markdown

--- sidebars.js
+++ sidebars.js
@@ -1,47 +1,53 @@
 module.exports = {
-	howSidebar: { 
-		Overview: [
-			'overview', 
-			'overview-code-of-conduct', 
-			'overview-molochv2-primer',  
-			'overview-raid-guild-shares',
-			'overview-roles'
-		],
-		GettingStarted: ['become-an-apprentice','become-a-member', 'rips','proposal-types'],
-		Raids: [
-			'convert-client-submission',
-			'commission-process',
-			'summon-a-raid-party',
-			'join-a-raid', 
-			'lead-a-raid',
-			 'shipped'],
-		Escrow: [
-			'how-does-it-work', 
-			'happy-path', 
-			'dispute', 
-			'interacting-with-escrow', 
-			'client-instructions', 
-			'references'
-		],
-		Discord: [
-			'discord-etiquette',
-			'discord-getting-started',
-			'discord-channels',
-			'discord-navigation-help',
-			
-		],
-		Community: [
-			'community-overview',
-			'community-meetings',
-			'community-forum',
-			'community-social-media'
-		],
-		AdditionalResources:[
-			'learn-about-web3',
-			'glossary',
-			'requesting-payment',
-			'migration-to-xdai',
-			'helping-docs'	
-		]
-	},
+  howSidebar: {
+    Overview: [
+      "overview",
+      "overview-code-of-conduct",
+      "overview-molochv2-primer",
+      "overview-raid-guild-shares",
+      "overview-roles",
+    ],
+    GettingStarted: [
+      "become-an-apprentice",
+      "become-a-member",
+      "rips",
+      "proposal-types",
+    ],
+    Raids: [
+      "convert-client-submission",
+      "commission-process",
+      "summon-a-raid-party",
+      "join-a-raid",
+      "lead-a-raid",
+      "shipped",
+    ],
+    Escrow: [
+      "how-does-it-work",
+      "happy-path",
+      "dispute",
+      "interacting-with-escrow",
+      "client-instructions",
+      "references",
+    ],
+    Discord: [
+      "discord-etiquette",
+      "discord-getting-started",
+      "discord-channels",
+      "discord-navigation-help",
+      "discord-bots",
+    ],
+    Community: [
+      "community-overview",
+      "community-meetings",
+      "community-forum",
+      "community-social-media",
+    ],
+    AdditionalResources: [
+      "learn-about-web3",
+      "glossary",
+      "requesting-payment",
+      "migration-to-xdai",
+      "helping-docs",
+    ],
+  },
 };
